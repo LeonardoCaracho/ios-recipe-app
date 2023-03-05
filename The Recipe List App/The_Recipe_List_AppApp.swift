27764ffd8fc460795420eb8bf32e6939040ae253
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-05 10:34:16 -0300

    Initial Commit

---
+++ The Recipe List App/The_Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Recipe_List_AppApp.swift
+//  The Recipe List App
+//
+//  Created by Leonardo Caracho on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
