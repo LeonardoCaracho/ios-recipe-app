ef76472c791e9f9c78864ae3072e8e71bb462a0b
Leonardo Caracho <leonardo.caracho@partner.bmw.com.br>
2023-03-07 19:46:16 -0300

    - Add RecipeFeaturedView - Modify data flow

---
+++ The Recipe List App/Views/RecipeFeaturedView.swift
@@ -0,0 +1,22 @@
+//
+//  RecipeFeaturedView.swift
+//  The Recipe List App
+//
+//  Created by Leonardo Caracho on 07/03/23.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    @EnvironmentObject var model:RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
